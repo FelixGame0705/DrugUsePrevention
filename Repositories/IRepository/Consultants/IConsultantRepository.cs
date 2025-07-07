77fe0d81d7e8ea65f2dbb2914bca10d41767f601
TienTran <tientrangame0705@gmail.com>
2025-07-07 21:01:38 +0700

    done tasks

--- Repositories/IRepository/Consultants/IConsultantRepository.cs
+++ Repositories/IRepository/Consultants/IConsultantRepository.cs
@@ -1,14 +1,12 @@
-﻿using BussinessObjects;
-using Repositories.Repository.Consultants;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BussinessObjects;
+using Repositories.Repository.Consultants;
 
 namespace Repositories.IRepository.Consultants
 {
-    public interface IConsultantRepository : IGenericRepository<Consultant>
-    {
-    }
+    public interface IConsultantRepository : IGenericRepository<Consultant> { }
 }
